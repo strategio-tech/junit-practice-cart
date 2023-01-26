2fd7a0dc3c02d6d38425e639cc5fcff96ac1dbb2
Eddie Espinosa <espinosa.edilson@gmail.com>
2023-01-26 12:06:35 -0600

    created test removeItemNotFound()

--- src/test/java/tech/strategio/shoppingCartTest.java
+++ src/test/java/tech/strategio/shoppingCartTest.java
@@ -1,19 +1,27 @@
 package tech.strategio;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class shoppingCartTest {
+  private ShoppingCart shoppingCart;
+
   @BeforeEach
   public void createCart() {
-    ShoppingCart shoppingCart = new ShoppingCart();
+    shoppingCart = new ShoppingCart();
   }
 
   @Test
   public void emptyTest() {
     assertEquals(true, true);
+  }
+
+  @Test
+  public void removeItemNotFound() throws ProductNotFoundException{
+    Product unreal = new Product("Golden Ticket", 1.99);
+    assertThrows(ProductNotFoundException.class, ()-> shoppingCart.removeItem(unreal));
   }
 }
