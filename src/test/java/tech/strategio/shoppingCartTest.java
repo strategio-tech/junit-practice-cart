fa186591cb68ee2992f298918dfd0fbae322f9ca
Eddie Espinosa <espinosa.edilson@gmail.com>
2023-01-26 11:42:41 -0600

    shoppingCartTest.java working empty test

---
+++ src/test/java/tech/strategio/shoppingCartTest.java
@@ -0,0 +1,19 @@
+package tech.strategio;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class shoppingCartTest {
+  @BeforeEach
+  public void createCart() {
+    ShoppingCart shoppingCart = new ShoppingCart();
+  }
+
+  @Test
+  public void emptyTest() {
+    assertEquals(true, true);
+  }
+}
