aeea13709d7d99d7c2beb95b9e1629de803d9f0a
Eddie Espinosa <espinosa.edilson@gmail.com>
2023-01-26 13:49:54 -0600

    merge omar branch

--- src/test/java/tech/strategio/shoppingCartTest.java
+++ src/test/java/tech/strategio/shoppingCartTest.java
@@ -30,4 +30,10 @@ public class shoppingCartTest {
     Product unreal = new Product("Golden Ticket", 1.99);
     assertThrows(ProductNotFoundException.class, ()-> shoppingCart.removeItem(unreal));
   }
+
+  @Test
+  public void isCartEmpty() {
+    shoppingCart.empty();
+    assertEquals(0, shoppingCart.getItemCount(), 0.0001);
+  }
 }
